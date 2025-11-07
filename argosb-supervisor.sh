f180cef4ff9299300ba236f6f0e334599353e001
playwjj <dc88@vip.qq.com>
2025-11-07 14:38:57 +0800

    Change script execution from argosb.sh to argosbx.sh

--- argosb-supervisor.sh
+++ argosb-supervisor.sh
@@ -3,6 +3,6 @@
 echo "[INFO] Starting ArgoSB Script (local)..."
 
 # 映射环境变量为脚本参数，然后执行本地脚本
-vmpt="$PORT" uuid="$UUID" argo="y" agn="$TUNNEL_DOMAIN" agk="$TUNNEL_TOKEN" bash /app/argosb.sh
+vmpt="$PORT" uuid="$UUID" argo="y" agn="$TUNNEL_DOMAIN" agk="$TUNNEL_TOKEN" bash /app/argosbx.sh
 
 tail -f /dev/null
